80bd47ad5f8440b80094de383b711551e5f44fbd
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 22:55:30 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,6 +2,22 @@ import streamlit as st
 
 st.set_page_config(page_title="Ứng dụng LSTM cho danh mục đầu tư",page_icon="📊")
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+add_bg_from_local('background.png')
 
 st.logo("logo.png",icon_image="logo.png")
 st.image("banner.png")
