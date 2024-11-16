8f95fbabcda4f5fe9b409cd2072d5bab66fab46a
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 23:00:34 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -13,6 +13,16 @@ def add_bg_from_local(image_file):
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
         background-size: cover;
     }}
+    .stApp::before {{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.5); /* Điều chỉnh độ trong suốt ở đây */
+        z-index: -1;
+    }}
     </style>
     """,
     unsafe_allow_html=True
